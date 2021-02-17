2d12a25f001790460a7b23ffaff59bff4e7f120b
andrdit <andrdit@gmail.com>
2021-02-17 21:43:49 +0300

    added home work text for test git from console

--- homework1/app/src/main/java/ru/andr/homework1/MainActivity.java
+++ homework1/app/src/main/java/ru/andr/homework1/MainActivity.java
@@ -11,4 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton.
+    Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты.*/
+
+    /*2. Создать ещё один макет (если не получается, то проект) с несколькими EditText,
+    где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения.*/
+
+
+   /* 3. Добавить в проект элемент CalendarView.*/
+
+    
+    /*4.* Разобраться, что такое параметр ems.*/
+
 }
