b58303965c3170c5e01d2e363a1a838410296288
andrdit <andrdit@gmail.com>
2021-02-27 23:05:02 +0300

    added buttons

--- Calculator/app/src/main/java/ru/andr/calculator/MainActivity.java
+++ Calculator/app/src/main/java/ru/andr/calculator/MainActivity.java
@@ -4,14 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtn0;
     private Button mBtn1;
+    private Button mBtn2;
+    private Button mBtn3;
+    private Button mBtn4;
+    private Button mBtn5;
+    private Button mBtn6;
+    private Button mBtn7;
+    private Button mBtn8;
+    private Button mBtn9;
+    private Button mBtnClear;
+    private Button mBtnBackSpace;
+    private Button mBtnProcents;
+    private Button mBtnDivide;
+    private Button mBtnMultiply;
+    private Button mBtnMinus;
+    private Button mBtnPlus;
+    private Button mBtnEquel;
+    private Button mBtnPt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.key_board_layout);
+
+        mBtn0 = (Button) findViewById(R.id.button0);
+        mBtn1 = (Button) findViewById(R.id.button1);
+        mBtn2 = (Button) findViewById(R.id.button2);
+        mBtn3 = (Button) findViewById(R.id.button3);
+        mBtn4 = (Button) findViewById(R.id.button4);
+        mBtn5 = (Button) findViewById(R.id.button5);
+        mBtn6 = (Button) findViewById(R.id.button6);
+        mBtn7 = (Button) findViewById(R.id.button7);
+        mBtn8 = (Button) findViewById(R.id.button8);
+        mBtn9 = (Button) findViewById(R.id.button9);
+        mBtnClear = (Button) findViewById(R.id.buttonClear);
+        mBtnBackSpace = (Button) findViewById(R.id.buttonBackSpace);
+        mBtnProcents = (Button) findViewById(R.id.buttonProcents);
+        mBtnDivide = (Button) findViewById(R.id.buttonDivide);
+        mBtnMultiply = (Button) findViewById(R.id.buttonMultiply);
+        mBtnMinus = (Button) findViewById(R.id.buttonMinus);
+        mBtnPlus = (Button) findViewById(R.id.buttonPlus);
+        mBtnEquel = (Button) findViewById(R.id.buttonEquel);
+        mBtnPt = (Button) findViewById(R.id.buttonPt);
     }
 }
