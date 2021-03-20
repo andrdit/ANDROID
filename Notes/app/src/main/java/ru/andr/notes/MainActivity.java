fc3794bfced215e98e54ec4df69d12596f9b461e
andrdit <andrdit@gmail.com>
2021-03-20 16:49:08 +0300

    добавил Navigation Drawer

--- Notes/app/src/main/java/ru/andr/notes/MainActivity.java
+++ Notes/app/src/main/java/ru/andr/notes/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.andr.notes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,6 +16,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -62,16 +68,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        initToolbar();
-//        initButtonMain();
-//        initButtonFavorite();
-//        initButtonSettings();
-//        initButtonBack();
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
     }
 
-    private void initToolbar() {
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    // регистрация drawer
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -104,26 +133,36 @@ public class MainActivity extends AppCompatActivity {
         // Обработка выбора пункта меню приложения (активити)
         int id = item.getItemId();
 
+        if (navigateFragment(id)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean navigateFragment(int id) {
         switch (id) {
             case R.id.action_settings:
                 //addFragment(new SettingsFragment());
                 Toast.makeText(MainActivity.this, "Go to settings fragment", Toast.LENGTH_SHORT).show();
                 return true;
-
             case R.id.action_favorite:
-                //addFragment(new FavoriteFragment());
-                Toast.makeText(MainActivity.this, "Go to favorite fragment", Toast.LENGTH_SHORT).show();
+                //addFragment(new MainFragment());
+                Toast.makeText(MainActivity.this, "Go to favorite fragment notes", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_add:
                 //addFragment(new FavoriteFragment());
                 Toast.makeText(MainActivity.this, "Add note", Toast.LENGTH_SHORT).show();
-                return true;
             case R.id.action_delete:
                 //addFragment(new FavoriteFragment());
                 Toast.makeText(MainActivity.this, "Delete note", Toast.LENGTH_SHORT).show();
                 return true;
+            case R.id.action_about:
+                //addFragment(new FavoriteFragment());
+                Toast.makeText(MainActivity.this, "About application", Toast.LENGTH_SHORT).show();
+                return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
 }
